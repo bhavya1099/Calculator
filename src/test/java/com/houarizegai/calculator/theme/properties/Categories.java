2e37acccabfe0a129bf7bc203b25e687f9e2b2c6
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-18 19:42:48 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/com/houarizegai/calculator/theme/properties/Categories.java
@@ -0,0 +1,81 @@
+package com.houarizegai.calculator.theme.properties;
+
+public interface Categories {
+
+	interface getName {
+
+	}
+
+	interface roostTestTag1 {
+
+	}
+
+	interface roostTestTag2 {
+
+	}
+
+	interface setName {
+
+	}
+
+	interface getApplicationBackground {
+
+	}
+
+	interface setApplicationBackground {
+
+	}
+
+	interface getTextColor {
+
+	}
+
+	interface setTextColor {
+
+	}
+
+	interface getBtnEqualTextColor {
+
+	}
+
+	interface setBtnEqualTextColor {
+
+	}
+
+	interface getOperatorBackground {
+
+	}
+
+	interface setOperatorBackground {
+
+	}
+
+	interface getNumbersBackground {
+
+	}
+
+	interface setNumbersBackground {
+
+	}
+
+	interface getBtnEqualBackground {
+
+	}
+
+	interface setBtnEqualBackground {
+
+	}
+
+	interface getThemes {
+
+	}
+
+	interface setThemes {
+
+	}
+
+	interface getThemesAsMap {
+
+	}
+
+}
---
+++ src/test/java/com/houarizegai/calculator/util/Categories.java
@@ -0,0 +1,17 @@
+package com.houarizegai.calculator.util;
+
+public interface Categories {
+
+	interface hex2Color {
+
+	}
+
+	interface roostTestTag1 {
+
+	}
+
+	interface roostTestTag2 {
+
+	}
+
+}
